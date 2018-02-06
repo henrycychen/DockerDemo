9d21def37a880563b7d8346373e8c15d5fcd969e
Henry <henrycychen@gmail.com>
2018-02-05 18:23:13 -0800

    Adding BaseTest.java

--- src/tests/BaseTest.java
+++ src/tests/BaseTest.java
@@ -1,24 +1,31 @@
 package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-class BaseTest {
+public class BaseTest {
+	public WebDriver driver;
+	public WebDriverWait wait;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		//Create a Chrome driver. All tests and page classes will use this.
+		driver = new ChromeDriver();
+		
+		//Create a wait. All tests and page classes will use this.
+		wait = new WebDriverWait(driver, 15);
+		
+		//Maximize Window
+		driver.manage().window().maximize();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		driver.quit();
 	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
 }

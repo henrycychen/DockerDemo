e08b8ccf275b5df43c5cef3e38517a6cdda9075f
Henry <henrycychen@gmail.com>
2018-02-05 18:40:32 -0800

    Adding basic methods for all pages to inherit

--- src/pages/BasePage.java
+++ src/pages/BasePage.java
@@ -1,5 +1,43 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/*
+ * BasePage class contains common methods for all page classes
+ */
+
 public class BasePage {
+	public WebDriver driver;
+	public WebDriverWait wait;
+	
+	//Constructor
+	public BasePage(WebDriver driver, WebDriverWait wait) {
+		this.driver = driver;
+		this.wait = wait;
+		System.out.println("BasePage initialized");
+	}
+	
+	//Click Method
+	public void click(String cssLocator) {
+		driver.findElement(By.cssSelector(cssLocator)).click();
+	}
+	
+	//Fill fields in text boxes
+	public void fillFields(String cssLocator, String text) {
+		driver.findElement(By.cssSelector(cssLocator)).sendKeys(text + "/n");
+	}
+	
+	//Get text from element
+	public String getText(String cssLocator) {
+		return driver.findElement(By.cssSelector(cssLocator)).getText();
+	}
+	
+	//Get text from attribute
+	public String getAttribute(String cssLocator, String attribute) {
+		return driver.findElement(By.cssSelector(cssLocator)).getAttribute(attribute);
+	}
+	
 
 }
